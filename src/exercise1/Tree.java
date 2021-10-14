2b2af06a365adc7db126104c5d14899772fbed7f
Rostislav DImov <91912721+midrost@users.noreply.github.com>
2021-10-14 10:54:07 +0300

    Fix test error

--- src/exercise1/Tree.java
+++ src/exercise1/Tree.java
@@ -2,6 +2,6 @@ package exercise1;
 
 public class Tree {
     public static void main(String[] args) {
-        System.out.print("    *     \n   ***   \n  *****  \n ******* \n*********\n   ***   ");
+        System.out.print("    *\n   ***\n  *****\n *******\n*********\n   ***");
     }
 }
